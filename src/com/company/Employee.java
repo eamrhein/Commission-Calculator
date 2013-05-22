32313c63a69df6ddba9897bf777c053b2e9763b6
eamrhein <eamrhein.important@gmail.com>
2013-05-22 11:43:05 -0700

    School Java Project to calcluate sales and comission of employees of a small buisness.

---
+++ src/com/company/Employee.java
@@ -0,0 +1,109 @@
+
+    /**
+     *  Week 3 Assignment
+     *  Eric Amrhein
+     *  Programing 420
+     *  Vikram Chowdiah
+     */
+    package com.company;
+    import java.math.BigDecimal;
+    import java.math.RoundingMode;
+    import java.util.Scanner;
+    public class Employee
+    {
+        /* Sets defaults for calculations */
+        double salary = 60000;
+        double commission = 0.10;
+        double bonusCommission = 0.15;
+        double salesTarget = 150000;
+        double bonusTime = salesTarget * 0.80;    // Incentives Kick in when 80% of the sales target is met
+
+        public BigDecimal money(double inputs)  //creats a method for Handling Money
+        {
+           return new BigDecimal(inputs).setScale(2,RoundingMode.HALF_EVEN); //sets scale and rounding mode
+        }
+
+        /*  --Totalcommission Method --
+         *  method that takes an employee's total sales and performs
+         *  calculations based on attributes set in Employee.java
+         *  The calculation changes based on whether or not employees
+         *  are within 80%  of the target goal
+         */
+        public BigDecimal Totalcommission (double totalSales)
+        {
+
+            if (totalSales < bonusTime)
+            {
+                double total = totalSales * this.commission;
+                double Compensation = total + this.salary;
+                return money(Compensation);
+            }
+            else if (totalSales>= bonusTime)
+            {
+                double total = totalSales*this.bonusCommission;
+                double Compensation = total + this.salary ;
+                return money(Compensation);
+            }
+            else
+            {
+                return (new BigDecimal(0));
+            }
+
+        }
+        /* -- GenerateTable Method --
+         *  print out a table that shows how commission would increase
+         *  when an employee's total sales increases by increments of
+         *  $5000
+         */
+        public BigDecimal GenerateTable(double totalSales)
+        {
+            System.out.println("Here is an Incentive table of possible commissions");
+            System.out.println("[Total Sales][Total Compensation]");
+            double counterEnd = (totalSales * 1.50); // loop end condition
+            double counterStart = totalSales;        // counter starts at input
+            while (counterStart<=counterEnd)        // loops until condition is met
+            {
+                if (counterStart < bonusTime)
+                {
+                    double commission = counterStart * this.commission;
+                    double Compensation = commission + this.salary;
+                    System.out.println(" $" + money(counterStart) + "      $" + money(Compensation));
+                }
+                else if (counterStart>= bonusTime)
+                {
+                    double commission = counterStart*this.bonusCommission;
+                    double Compensation = commission + this.salary;
+                    System.out.println(" $" + money(counterStart) + "      $" + money(Compensation));
+                }
+                counterStart = counterStart +5000;
+            }
+            return (new BigDecimal(0));
+        }
+
+        /* --Generate Input Method
+         *
+         */
+
+        public double promptForSales()
+        {
+            Scanner input = new Scanner(System.in);  //create new Scanner object called input
+            System.out.print("Please enter your total company sales for the year:"); //One and Only User Prompt
+
+            while (!input.hasNextDouble())
+            {
+                System.out.println("Please Enter Only Numbers");
+                input.next();
+            }
+
+            return input.nextDouble(); //Store input for future use
+        }
+
+        public  String GenerateComp(double input)
+        {
+            BigDecimal AnnualComp = this.Totalcommission(input); //calls Totalcommison() Calculator method for actual calculations
+            System.out.println("Your Total Annual Compensation is: $" + AnnualComp);
+            System.out.println(" "); //Create a line skip for easier reading
+            return " ";
+        }
+    }
+
---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+/**
+ *  Week 3 Assignment
+ *  Eric Amrhein
+ *  Programing 420
+ *  Vikram Chowdiah
+ */
+
+package com.company; //Package Directory, Remove if File Directories are changed
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Employee salesman = new Employee(); //create new Employee class
+        double total = salesman.promptForSales();
+        salesman.GenerateComp(total);
+        salesman.GenerateTable(total);
+    }
+}
